4e84e2c02bcb9853c7d634eb9f76876d7aaecc8d
Imad Assouabi <imadassouaby05@gmail.com>
2025-03-13 12:42:59 +0000

    Delete data/Dataloader.py

--- data/Dataloader.py
+++
@@ -1,5 +0,0 @@
-def download_data():
-  data_local_filename = 'heart_failure_clinical_records_dataset.csv'
-    url = "https://archive.ics.uci.edu/dataset/519/heart%2Bfailure%2Bclinical%2Brecords"
-    urllib.request.urlretrieve(url, data_local_filename)
-    return delivery_data_local_filename
