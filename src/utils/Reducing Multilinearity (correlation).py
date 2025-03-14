8d861295bf80f180381ee191bd52a65ee787dd1c
Ouamalich Aasmaa <aasmaa.ouamalich.cycle.ing@gmail.com>
2025-03-14 11:11:01 +0000

    Add files via upload

---
+++ src/utils/Reducing Multilinearity (correlation).py
@@ -0,0 +1,27 @@
+# Reducing Multilinearity (correlation)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from scipy.stats import boxcox
+from scipy.stats.mstats import winsorize
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from dataloader import download_data
+from data_processing import optimise_memory
+
+
+# Reading data
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+df.info()
+
+df.describe()
+
+# Heatmap to visualize correlation between features
+
+plt.figure(figsize=(18,18))
+sns.heatmap(df.corr(),annot=True,cmap='crest')
+
+plt.show()
