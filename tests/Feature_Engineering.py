5c90715814b79e4d6ec59f7a715b2a169277acbe
Lucien Desmoulins <desmoulinslucien@gmail.com>
2025-03-16 00:39:05 +0000

    Update Feature_Engineering.py

--- tests/Feature_Engineering.py
+++ tests/Feature_Engineering.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from scipy.stats import boxcox
+from scipy.stats.mstats import winsorize
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from data_processing import optimise_memory
+
